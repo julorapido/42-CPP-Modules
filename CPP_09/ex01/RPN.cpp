9903331828d4c1eec980d9c060514923b13d6b12
Jules Sainthorant <jsaintho@bess-f4r6s4.clusters.42paris.fr>
2025-02-14 14:44:58 +0100

    fix SegFaults in RPN special cases CPP_09/ex01

--- CPP_09/ex01/RPN.cpp
+++ CPP_09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: jsaintho <jsaintho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 12:58:31 by jsaintho          #+#    #+#             */
-/*   Updated: 2025/02/13 16:50:50 by jsaintho         ###   ########.fr       */
+/*   Updated: 2025/02/14 14:42:42 by jsaintho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,27 @@ RPN::RPN(const RPN &copy)
 }
 int RPN::verif_args(int a, char **argv)
 {
+    std::string q = std::string("*+-/");
     for(int i = 1; i < a; i++)
     {
         std::string s = std::string(argv[i]);
+        if(s.length() < 5)
+            return 7;
+        else
+        {
+            if (!(std::isdigit(s[0]) && std::isdigit(s[2])
+                    && s[1] == ' ')
+            )
+                return 8;
+        }
+        int o = 0, n = 0;
         for (size_t j = 0; j < s.length(); ++j)
         {
+            if(std::isdigit(s[j]))
+                n++;
+            if(s[j] == '*' || s[j] == '-'
+                    || s[j] == '+' || s[j] == '/')
+                o++;
             if (!std::isdigit(s[j]) && !(
                     s[j] == '*' || s[j] == '-'
                     || s[j] == '+' || s[j] == '/'
@@ -52,9 +68,28 @@ int RPN::verif_args(int a, char **argv)
                 if(s[j + 2] && s[j + 4])
                     if(std::isdigit(s[j]) && std::isdigit(s[j + 2]) && std::isdigit(s[j + 4]))
                         return 4;
+                // "1 ** 2"
+                if(s[j + 1])
+                    if((q.find(s[j + 1]) != std::string::npos)
+                        && q.find(s[j]) != std::string::npos)
+                            return 5;
+                // "1 * / 2 + 6 +"
+                if (s[j + 2])
+                    if((q.find(s[j + 2]) != std::string::npos
+                        && q.find(s[j]) != std::string::npos)
+                        && (j + 2 != s.length() - 1))
+                            return 6;
             }
         }
+        //std::cout << s << "has " << o<< "-operators and " << n << "-numbers" << std::endl; 
+        if(o == n)
+            return (9);
         int k = s.length() - 1;
+        if (( s[k] == '*' || s[k] == '-'
+            || s[k] == '+' || s[k] == '/'
+        )) return (0);
+        for(; k > 0 && s[k] == ' '; k --)
+        {}
         if (!( s[k] == '*' || s[k] == '-'
             || s[k] == '+' || s[k] == '/'
             ))
@@ -93,6 +128,8 @@ void    RPN::stack_args(int a, char **argv)
 
 void    RPN::handle_args(int a, char **argv)
 {
+    std::string q = std::string("*+-/");
+
     for(int j = 1; j < a; j++)
     {
         std::string str = std::string(argv[j]);
@@ -106,33 +143,54 @@ void    RPN::handle_args(int a, char **argv)
             }
             else // operator
             {
+            
                 if(str[i] != ' ')
                 {
-                    int c[2] = {0, 0};
-                    for(int i = 0; i < 2; i ++)
-                    {
-                        c[i] = this->s.top();
-                        this->s.pop();
-                    }
-                    std::cout << c[1] << str[i] << c[0] << std::endl;    
-                    if(c[0] != 0 && c[1] != 0)
-                    {
-                        switch (str[i])
+                    // special case "1 2 + 1 * -" 
+                    // if(i == str.length() - 1)
+                    // {
+                    //     int p = i;
+                    //     for(; p > 0 && !(q.find(str[p]) != std::string::npos 
+                    //         || std::isdigit(str[p])); p --)
+                    //     { }
+                    //     if(q.find(str[p]) != std::string::npos)
+                    //     {
+                    //         //std::cout << "wer "<< str[i] << "(" << this->s.top() << ")" << std::endl; 
+                    //         if(str[i] == '+' || str[i] == '-')
+                    //         {
+                    //             int n_top = this->s.top();
+                    //             this->s.push();
+                    //         }
+                    //         return ;
+                    //     }
+                    // }else
+                    // {
+                        int c[2] = {-1, -1};
+                        for(int i = 0; i < 2; i ++)
                         {
-                            case '+':
-                                this->s.push(c[0] + c[1]);
-                                break;
-                            case '-':
-                                this->s.push(c[1] - c[0]);
-                                break;
-                            case '*':
-                                this->s.push(c[0] * c[1]);
-                                break;
-                            case '/':
-                                this->s.push(c[1] / c[0]);
-                                break;
+                            c[i] = this->s.top();
+                            this->s.pop();
                         }
-                    }
+                        std::cout << c[1] << str[i] << c[0] << std::endl;    
+                        if(c[0] != -1 && c[1] != -1)
+                        {
+                            switch (str[i])
+                            {
+                                case '+':
+                                    this->s.push(c[0] + c[1]);
+                                    break;
+                                case '-':
+                                    this->s.push(c[1] - c[0]);
+                                    break;
+                                case '*':
+                                    this->s.push(c[0] * c[1]);
+                                    break;
+                                case '/':
+                                    this->s.push(c[1] / c[0]);
+                                    break;
+                            }
+                        }
+                    //}
                 }
             }
         }
--- CPP_09/ex01/main.cpp
+++ CPP_09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jsaintho <jsaintho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 12:58:36 by jsaintho          #+#    #+#             */
-/*   Updated: 2025/02/13 16:52:00 by jsaintho         ###   ########.fr       */
+/*   Updated: 2025/02/14 14:43:57 by jsaintho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,26 @@ int main(int argc, char** argv)
     z = ReversePolishNotation->verif_args(argc, argv);
     if(z != 0)
     {
+        std::cout << "\033[33m";
         if(z == 3)
             std::cout << "Error: 'notation should finish with an operator.' [+*/-]" << std::endl;
         else if (z == 2)
             std::cout << "Error: 'numbers should be spaced.' [123 => 1 2 3]" << std::endl;
+        else if (z == 5)
+            std::cout << "Error: 'operators should be spaced.' [1 5 ** 2 => 1 5 * 2]" << std::endl;
         else if (z == 4)
             std::cout << "Error: 'only 2 consecutive numbers' [1 8 3 + 4 - => 1 8 + 4 -]" << std::endl;
+        else if (z == 6)
+            std::cout << "Error: 'consecutive operators can only be placed at the end.' [1 5 + - 2 + => 1 5 + 2 + -]" << std::endl;
+        else if (z == 7)
+            std::cout << "Error: 'at least 2-operands 1-operation polish notation.' " << std::endl;    
+        else if (z == 8)
+            std::cout << "Error: 'notation should start with 2-spaced-operands.' " << std::endl;  
+        else if (z == 9)  
+            std::cout << "Error: 'cant have even number of operands/operators.' " << std::endl;  
         else                
             std::cout << "Error: args only: [0-9][*, -, +, /]" << std::endl;
+        std::cout << "\033[0m";
         return (1);
     }
     // ReversePolishNotation->stack_args(argc, argv);
